11fdd642b4edb7a2205963d07e24830091785ab9
崔箐坡 <hbucqp1991@sina.cn>
2024-08-28 13:00:03 +0800

    update

--- build/lib/cvmcore/__init__.py
+++ build/lib/cvmcore/__init__.py
@@ -3,7 +3,7 @@
 __title__ = 'cvmbcore'
 __description__ = 'core function of data analysis used by SZQ lab from China Agricultural University'
 __url__ = 'https://github.com/hbucqp/cvmcore'
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = 'Qingpo Cui'
 __author_email__ = 'cqp@cau.edu.cn'
 __license__ = 'MIT'
--- cvmcore/cvmcore.py
+++ cvmcore/cvmcore.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 # data process
 import dask.array as da
@@ -35,6 +36,55 @@ from typing import Optional, List, Dict, Union, Tuple
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = ['Arial']
 plt.rcParams['svg.fonttype'] = 'none'
+
+
+class cfunc():
+    pass
+
+    @staticmethod
+    def is_fasta(file):
+    """
+    chcek if the input file is fasta format
+    """
+        try:
+            with open(file, "r") as handle:
+                fasta = SeqIO.parse(handle, "fasta")
+                # False when `fasta` is empty, i.e. wasn't a FASTA file
+                return any(fasta)
+        except:
+            return False
+
+    def get_mod_time(file):
+    """
+    Return the last modified time of file as YYYY-MM-DD string format.
+    Parameters
+    ----------
+    file :
+        file path string
+    Returns
+    ----------
+    Raises
+    ----------
+    Notes
+    ----------
+    References
+    ----------
+    See Also
+    ----------
+    Examples
+    ----------
+    """
+        file = os.path.abspath(file)
+        md_time = os.stat(file).st_mtime
+        lst_mod_time = time.strftime("%Y-%m-%d", time.localtime(md_time))
+        return lst_mod_time
+
+    def get_rel_path():
+    """
+    Get the relative path
+    """
+        here = os.path.abspath(os.path.dirname(__file__))
+        return here
 
 
 class cvmbox():
